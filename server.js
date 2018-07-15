e2d1dc6c6d1fb97b61655806dc23510ba0993103
Shaan Hashmi <shaanh33@gmail.com>
2018-07-16 02:28:03 +0530

    deploy to heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/A6-B4'));
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/A6-B4/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
