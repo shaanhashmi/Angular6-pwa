9fac313e4bf537edd8be23a7565bcf6791b00c8c
Shaan Hashmi <shaanh33@gmail.com>
2018-09-01 20:36:32 +0530

    UI fix

--- src/app/pages/login-page/login-page.module.ts
+++ src/app/pages/login-page/login-page.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'
 import { LoginPageComponent } from './login-page.component';
 import { ComponentsModule } from '../../components/components.module';
- 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    RouterModule.forChild([{path:'', component: LoginPageComponent}]),
-    ComponentsModule
+    RouterModule.forChild([{ path: '', component: LoginPageComponent }]),
+    ComponentsModule,
+    FormsModule
   ],
   declarations: [LoginPageComponent],
-  exports:[LoginPageComponent],
-  entryComponents:[LoginPageComponent]
+  exports: [LoginPageComponent],
+  entryComponents: [LoginPageComponent]
 })
 export class LoginPageModule { }
