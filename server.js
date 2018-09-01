b691ee3f4467ba54f708bc55d4dddf7668294556
Shaan Hashmi <shaanh33@gmail.com>
2018-09-01 15:30:18 +0530

    project name change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/A6-B4'));
+app.use(express.static(__dirname + '/dist/stackdevs'));
 
 // app.get('/*', function (req, res) {
 //     res.sendFile(path.join(__dirname + '/dist/A6B4/index.html'));
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to A6-B4!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to stackdevs!');
   }));
 });
